8c6c333f5f277b44addcae17ffc9f4095bbd2473
Marializ <166574086+MzMarializ@users.noreply.github.com>
2026-08-13 20:45:08 -0400

    Agregando analisis y verif Co-authored-by-<MarializDisla>

--- src/Program.cs
+++ src/Program.cs
@@ -1,7 +1,7 @@
 using AnalizadorArchivos;
 using System.Diagnostics;
 
-string carpeta = @"C:\Users\antho\OneDrive\Documentos\Pruebas txt\libros_espanol";
+string carpeta = @"C:\Users\maria\OneDrive\Desktop\Paralela\Finalfinal\DocumentosAnalizar";
 
 // Verificacion de carpeta
 if (!Directory.Exists(carpeta))
@@ -60,8 +60,36 @@ Console.WriteLine($"Total de lineas:     {r1.TotalLineas:N0}");
 Console.WriteLine($"Total de palabras:   {r1.TotalPalabras:N0}");
 Console.WriteLine($"Total de caracteres: {r1.TotalCaracteres:N0}");
 
+Console.WriteLine("-------------------------------------------");
 Console.WriteLine($"Coincidencias de terminos clave: {r1.TotalCoincidencias:N0}");
+Console.WriteLine("-------------------------------------------");
 Console.WriteLine("Top 3 palabras mas repetidas:");
 foreach (KeyValuePair<string, int> par in r1.TopPalabras(3))
     Console.WriteLine($"   {par.Key} -> {par.Value:N0}");
+
+//Analisis de rendimiento
+
+Console.WriteLine("---------------------------------");
+Console.WriteLine("-----Analisis de Rendimiento-----");
+Console.WriteLine("---------------------------------");
+
+Console.WriteLine($"Nucleos disponibles: {Environment.ProcessorCount}\n");
+
+Console.WriteLine($"Modo Secuencial:        {t1,8:N1} ms   (speedup 1.00x, eficiencia 100%)");
+Console.WriteLine($"Modo Paralelo x archivo:{t2,8:N1} ms  (speedup {t1 / t2:N2}x, eficiencia {(t1 / t2) / grupos * 100:N0}%)");
+Console.WriteLine($"Modo Paralelo x grupos: {t3,8:N1} ms   (speedup {t1 / t3:N2}x, eficiencia {(t1 / t3) / grupos * 100:N0}%)");
+
+//verificacion
+Console.WriteLine("---------------------------------");
+Console.WriteLine("Verificacion:                    ");
+Console.WriteLine("---------------------------------");
+
+Console.WriteLine($"  Secuencial:   {r1.TotalPalabras:N0} palabras");
+Console.WriteLine($"  Por archivo:  {r2.TotalPalabras:N0} palabras");
+Console.WriteLine($"  Por grupos:   {r3.TotalPalabras:N0} palabras");
+Console.WriteLine($"  Coincidencias: {r1.TotalCoincidencias:N0} = {r2.TotalCoincidencias:N0} = {r3.TotalCoincidencias:N0}");
+
+Console.WriteLine("\nPresiona ENTER para salir...");
+Console.ReadLine();
+
 
